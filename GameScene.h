60fde561307da8c224b45dd6dcc77116c9e497e2
Nikita Gudkov <nikitaster2001@gmail.com>
2020-06-06 20:29:52 +0300

    replace GameScene, GameOverScene classes to separate files

---
+++ GameScene.h
@@ -0,0 +1,26 @@
+#ifndef GameScene_H
+#define GameScene_H
+
+#include "GameView.h"
+
+class GameScene {
+	GameModel * game_model;
+	GameController * game_controller;
+	GameView * game_view;
+public:
+	GameScene();
+
+	inline bool & onPause() { return this->game_view->onPause; }
+	inline bool & isGameOver() { return this->game_view->isGameOver; }
+
+	/*void setNewGame() {
+	game_model->end_code = -1;
+	game_model = new GameModel();
+	game_controller = new GameController(*game_model);
+	game_view = new GameView(*game_model, *game_controller);
+	}*/
+
+	void run(sf::RenderWindow &window);
+};
+
+#endif
