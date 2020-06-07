3944b000acd00b5b264ec5e6ee693c1dfb04d244
Borisenko Daria <dashulyabor@gmail.com>
2020-06-07 20:29:19 +0300

    adding about page with text and disabled controllers

--- main.cpp
+++ main.cpp
@@ -8,22 +8,29 @@
 
 class MenuScene : public BaseScene {
 	sf::Sprite menu;
+	sf::Sprite about_page;
 	bool is_menu = 1;
 	int button_num;
 	sf::Texture menu_texture;
+	sf::Texture about_texture;
 	sf::Font font_for_game;
 	sf::Font font_for_buttons;
 	sf::Text game;
 	sf::Text start_button;
 	sf::Text about_project_button;
 	sf::Text exit_button;
+	sf::Text controllers_button;
+	sf::Text back_to_menu;
 
 public:
 	MenuScene() {
 		start_button.setPosition(281, 123);
 		about_project_button.setPosition(242, 183);
-		exit_button.setPosition(353, 243);
+		exit_button.setPosition(353, 303);
 		game.setPosition(245, 53);
+		about_page.setPosition(60, 138);
+		controllers_button.setPosition(264, 243);
+		back_to_menu.setPosition(248, 512);
 
 		this->is_menu = 1;
 		this->button_num = 0;
@@ -31,23 +38,31 @@ public:
 		font_for_game.loadFromFile("font2.ttf");
 		font_for_buttons.loadFromFile("font3.ttf");
 		menu_texture.loadFromFile("menu_background.png");
+		about_texture.loadFromFile("about_page.png");
 
 		menu.setTexture(menu_texture);
+		about_page.setTexture(about_texture);
 
 		start_button.setFont(font_for_buttons);
 		about_project_button.setFont(font_for_buttons);
 		exit_button.setFont(font_for_buttons);
 		game.setFont(font_for_game);
+		controllers_button.setFont(font_for_buttons);
+		back_to_menu.setFont(font_for_buttons);
 
 		game.setCharacterSize(60);
 		start_button.setCharacterSize(50);
 		about_project_button.setCharacterSize(50);
 		exit_button.setCharacterSize(50);
+		controllers_button.setCharacterSize(50);
+		back_to_menu.setCharacterSize(30);
 
 		start_button.setString("start game");
 		about_project_button.setString("about project");
 		exit_button.setString("exit");
 		game.setString("Arkanoid");
+		controllers_button.setString("controllers");
+		back_to_menu.setString("PRESS esc TO EXIT TO MENU");
 	}
 
 	inline bool & get_menu_statement() { return is_menu; };
@@ -74,12 +89,14 @@ public:
 		start_button.setFillColor(sf::Color(61, 31, 115));
 		about_project_button.setFillColor(sf::Color(61, 31, 115));
 		exit_button.setFillColor(sf::Color(61, 31, 115));
+		controllers_button.setFillColor(sf::Color(61, 31, 115));
 		game.setFillColor(sf::Color(61, 31, 115));
 		this->menu_logic(window);
 		window.draw(menu);
 		window.draw(start_button);
 		window.draw(about_project_button);
 		window.draw(exit_button);
+		window.draw(controllers_button);
 		window.draw(game);
 		window.display();
 	}
@@ -101,18 +118,23 @@ public:
 			this->about_project_button.setFillColor(sf::Color::Blue);
 			this->button_num = 2;
 		}
-		else if (sf::IntRect(353, 267, 94, 28).contains(sf::Mouse::getPosition(window))) {
-			this->exit_button.setFillColor(sf::Color::Blue);
+		else if (sf::IntRect(264, 267, 272, 28).contains(sf::Mouse::getPosition(window))) {
+			this->controllers_button.setFillColor(sf::Color::Blue);
 			this->button_num = 3;
+		}
+		else if (sf::IntRect(353, 327, 94, 28).contains(sf::Mouse::getPosition(window))) {
+			this->exit_button.setFillColor(sf::Color::Blue);
+			this->button_num = 4;
 		}
 		else
 			this->button_num = 0;
 	}
 
-	void about_page(sf::RenderWindow &window) {
+	void about_page_scene(sf::RenderWindow &window) {
 		window.draw(menu);
 		window.draw(game);
-		//  about_project
+		window.draw(about_page);
+		window.draw(back_to_menu);
 		window.display();
 		while (!sf::Keyboard::isKeyPressed(sf::Keyboard::Escape));
 	}
@@ -123,9 +145,12 @@ public:
 			if (button_num == 1)
 				this->is_menu = false;
 			if (button_num == 2) {
-				this->about_page(window);
+				this->about_page_scene(window);
 			}
 			if (button_num == 3) {
+				//this->about_page_scene(window);
+			}
+			if (button_num == 4) {
 				this->is_menu = false;
 				window.close();
 			}
@@ -388,7 +413,7 @@ public:
 		runners = { {"menu", std::bind(&MainView::show_menu, this)},
 					{"game", std::bind(&MainView::show_game, this)},
 					{ "pause", std::bind(&MainView::show_pause, this) },
-					{ "gameOver", std::bind(&MainView::show_game_over, this) }, 
+					{ "gameOver", std::bind(&MainView::show_game_over, this) },
 		};
 		current_runner = "menu";
 	}
@@ -460,7 +485,7 @@ public:
 				if (over_scene->getButton() == 1 && over_scene->getButtonStatus())
 				{
 					reset_game_scene();
-					current_runner = "menu"; 
+					current_runner = "menu";
 					menu_scene->get_menu_statement() = true;
 					sf::sleep(sf::milliseconds(100));
 				}
