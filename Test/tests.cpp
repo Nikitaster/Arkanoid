680aef624e64308cafdefaf08c6c7d6bf0c6cb90
Mamonova Ksenia <djuvia.loxar@yandex.ru>
2020-06-17 13:57:09 +0300

    Fixed tests

--- Test/tests.cpp
+++ Test/tests.cpp
@@ -1,22 +1,63 @@
 #include "catch.hpp"
-//#include "C:\Users\AicShy\source\repos\bruh\project-arkanoid\Arkanoid\Classes\Velocity.cpp"
+#include "../Arkanoid/Classes/MainView.h"
 
 using namespace Catch::Matchers;
 
-//SCENARIO("Velocity has default constructor", "[velocity]") {
-//
-//    GIVEN("Velocity object with default params") {
-//        Velocity velocity;
-//        REQUIRE(velocity.getDirection() == 90);
-//
-//        WHEN("velocity change direction") {
-//            velocity.setDirection(27);
-//
-//            THEN("the direction change") {
-//                REQUIRE(velocity.getDirection() == 27);
-//            }
-//        }
-//    }
-//}
+SCENARIO("Velocity", "[velocity]") {
+    GIVEN("Velocity object with default params") {
+        Velocity velocity;
+        REQUIRE(velocity.getDirection() == 90);
 
-//TEST_CASE
+        WHEN("velocity change direction") {
+            velocity.setDirection(27);
+
+            THEN("the direction changes") {
+                REQUIRE(velocity.getDirection() == 27);
+            }
+        }
+    }
+    GIVEN("Velocity object with new params") {
+        Velocity velocity(30);
+        REQUIRE(velocity.getDirection() == 30);
+
+        WHEN("velocity reverse") {
+            velocity.reverse();
+
+            THEN("the direction reverses") {
+                REQUIRE(velocity.getDirection() == -150);
+            }
+        }
+
+        WHEN("velocity reverse x") {
+            velocity.reverseX();
+
+            THEN("the direction reverses on x axis") {
+                REQUIRE(velocity.getDirection() == 150);
+            }
+        }
+
+        WHEN("velocity reverse y") {
+            velocity.reverseY();
+
+            THEN("the direction reverses on y axis") {
+                REQUIRE(velocity.getDirection() == -30);
+            }
+        }
+    }
+}
+
+TEST_CASE("rect") {
+    sf::RectangleShape rect(sf::Vector2f(20, 10));
+    REQUIRE(rect.getSize() == sf::Vector2f(20, 10));
+}
+
+TEST_CASE("puck") {
+    Puck puck;
+    REQUIRE(puck.isMoving() == true);
+    REQUIRE(puck.isDead == false);
+}
+
+TEST_CASE("brick pile") {
+    BrickPile pile;
+    REQUIRE(pile.bricks.size() == 28);
+}
